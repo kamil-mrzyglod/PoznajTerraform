8c37e55e5c406e5a169ff4b1d9e4a1b4bd37040b
Kamil Mrzygłód <kamil-mrzyglod@users.noreply.github.com>
2023-07-25 12:43:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "filename" {
-  type = string
-  default = "hello.txt"
+variable "files" {
+  type = map(object({
+    filename          = string
+    content           = string
+    is_base64_content = bool
+    file_permission   = string
+    is_sourced_file   = bool
+    file_source       = string
+    is_secret         = bool
+  }))
+  description = "List of files to create."
 }
