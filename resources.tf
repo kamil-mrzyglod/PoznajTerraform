df51b1ef48b8879e43c11c9348458ab4f3347602
Kamil Mrzygłód <kamil-mrzyglod@users.noreply.github.com>
2023-07-25 12:43:10 +0200

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,4 +1,19 @@
-resource "local_file" "file" {
-  content  = "Hello, World!"
-  filename = var.filename
+module "local-file" {
+  source  = "app.terraform.io/PoznajTerraformExercise/local-file/terraform"
+  version = "1.0.0"
+
+  for_each = var.files
+
+  common = {
+    filename        = each.value.filename
+    file_permission = each.value.file_permission
+  }
+
+  content = {
+    file_content      = each.value.content
+    is_base64_content = each.value.is_base64_content
+    is_sourced_file   = each.value.is_sourced_file
+    file_source       = each.value.file_source
+    is_secret         = each.value.is_secret
+  }
 }
